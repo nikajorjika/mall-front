0bf4a23f97a8cd34ef5860ca6a17d8cd5fda0887
Nika Jorjoliani <nikajorjika1993@gmail.com>
2018-11-15 00:48:42 +0400

    added registration page

---
+++ dist/precache-manifest.3175ffef98484a264eb19ff3ef3e2212.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
+    "url": "/fonts/OpenSans-Bold.f5331cb6.ttf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "26ec7406f51c5efb79ecc2885a5b887c",
+    "url": "/fonts/bpg-nino-mtavruli-webfont.26ec7406.eot"
+  },
+  {
+    "revision": "3b0c11ba426b86c9bbf9",
+    "url": "/css/chunk-0a801736.3f9e8ba3.css"
+  },
+  {
+    "revision": "d2a526b19eb794abad94",
+    "url": "/css/chunk-16a7cb6e.b819a85f.css"
+  },
+  {
+    "revision": "d2a526b19eb794abad94",
+    "url": "/js/chunk-16a7cb6e.5272c2ad.js"
+  },
+  {
+    "revision": "c275a7e054a0a7ba5829",
+    "url": "/css/chunk-1d3b7328.32900a92.css"
+  },
+  {
+    "revision": "c275a7e054a0a7ba5829",
+    "url": "/js/chunk-1d3b7328.d31c0b17.js"
+  },
+  {
+    "revision": "29110e1b4fa1013e456d",
+    "url": "/css/chunk-2e69763b.915180e3.css"
+  },
+  {
+    "revision": "29110e1b4fa1013e456d",
+    "url": "/js/chunk-2e69763b.e030e449.js"
+  },
+  {
+    "revision": "b6fde7b5bb4da64555ec",
+    "url": "/css/chunk-316c0490.f5eeed6a.css"
+  },
+  {
+    "revision": "b6fde7b5bb4da64555ec",
+    "url": "/js/chunk-316c0490.2749f59d.js"
+  },
+  {
+    "revision": "1679c52aa4d074832db8",
+    "url": "/css/chunk-6494c454.dc25da61.css"
+  },
+  {
+    "revision": "1679c52aa4d074832db8",
+    "url": "/js/chunk-6494c454.47d2c574.js"
+  },
+  {
+    "revision": "2b53537aefa3d70995d3",
+    "url": "/css/chunk-7c4e1a49.17660906.css"
+  },
+  {
+    "revision": "2b53537aefa3d70995d3",
+    "url": "/js/chunk-7c4e1a49.89ed94d4.js"
+  },
+  {
+    "revision": "31e0fd267541bab63b48",
+    "url": "/css/chunk-7cf750f0.2e0723e6.css"
+  },
+  {
+    "revision": "31e0fd267541bab63b48",
+    "url": "/js/chunk-7cf750f0.713f1888.js"
+  },
+  {
+    "revision": "76ec6061db0736892111",
+    "url": "/css/chunk-vendors.d44e3af1.css"
+  },
+  {
+    "revision": "76ec6061db0736892111",
+    "url": "/js/chunk-vendors.b377e8a1.js"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/fonts/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "9ff12f694e5951a6f51a9d63b05062e7",
+    "url": "/fonts/OpenSans-Light.9ff12f69.ttf"
+  },
+  {
+    "revision": "e1c83f9474e0cc1d84a13c6d1ddf3ca5",
+    "url": "/fonts/OpenSans-SemiBold.e1c83f94.ttf"
+  },
+  {
+    "revision": "49f89e34d03233b1f27788f75df7a40a",
+    "url": "/fonts/OpenSans-ExtraBold.49f89e34.ttf"
+  },
+  {
+    "revision": "0b77837d187e20c1cc90",
+    "url": "/js/app.638cf2ee.js"
+  },
+  {
+    "revision": "3b0c11ba426b86c9bbf9",
+    "url": "/js/chunk-0a801736.1f64c2b6.js"
+  },
+  {
+    "revision": "e03e2b7b3b76e334d11caf2b1639e6a9",
+    "url": "/fonts/bpg-nino-mtavruli-webfont.e03e2b7b.woff2"
+  },
+  {
+    "revision": "9c57740b6f314b7db24f8cddde741770",
+    "url": "/fonts/bpg-nino-mtavruli-webfont.9c57740b.woff"
+  },
+  {
+    "revision": "afcdb491132664da5cc93fae552e72be",
+    "url": "/fonts/bpg-nino-mtavruli-webfont.afcdb491.ttf"
+  },
+  {
+    "revision": "97a80250cdafef79da7329c445faeaa6",
+    "url": "/img/bpg-nino-mtavruli-webfont.97a80250.svg"
+  },
+  {
+    "revision": "6e5b1494e933ba58cdb6e80876634552",
+    "url": "/fonts/bpg-arial-webfont.6e5b1494.woff2"
+  },
+  {
+    "revision": "a8b935b9fab81016cc4b241faf9a2260",
+    "url": "/fonts/bpg-arial-webfont.a8b935b9.eot"
+  },
+  {
+    "revision": "e1412bac33d65a665ecb4a78c4588cbd",
+    "url": "/fonts/bpg-arial-webfont.e1412bac.woff"
+  },
+  {
+    "revision": "6530709ce0aadf3283db6512a8fcabec",
+    "url": "/fonts/bpg-arial-webfont.6530709c.ttf"
+  },
+  {
+    "revision": "88a37bc21733e2fef7deba65d7271bed",
+    "url": "/img/bpg-arial-webfont.88a37bc2.svg"
+  },
+  {
+    "revision": "877c40403eb04862f727a57c090855f5",
+    "url": "/img/youtube.877c4040.svg"
+  },
+  {
+    "revision": "cf5d2bd997faa249bd7605fce51f232c",
+    "url": "/img/pinterest.cf5d2bd9.svg"
+  },
+  {
+    "revision": "8aca5902f38e7183fdbbab686a391786",
+    "url": "/img/facebook.8aca5902.svg"
+  },
+  {
+    "revision": "5b2501b16a00b646b815d66c5db0e81f",
+    "url": "/img/arrow-left-2.5b2501b1.svg"
+  },
+  {
+    "revision": "c716539f72517987354d428a99c7cec1",
+    "url": "/img/twitter.c716539f.svg"
+  },
+  {
+    "revision": "60c4abc1c30a5ad01448dba22276f44f",
+    "url": "/img/arrow-left-dark.60c4abc1.svg"
+  },
+  {
+    "revision": "99508fdde34a93abaf824338e329f637",
+    "url": "/img/carret-down.99508fdd.svg"
+  },
+  {
+    "revision": "19621429770ce665c670db0beb9588ac",
+    "url": "/img/TM_LOGO.19621429.svg"
+  },
+  {
+    "revision": "97b2b1f258f047fa27a7f8c2131192f9",
+    "url": "/img/tb_mall_logo_black.97b2b1f2.svg"
+  },
+  {
+    "revision": "15afb927d448415fad6c9bb25627ffcb",
+    "url": "/img/arrow-right-2.15afb927.svg"
+  },
+  {
+    "revision": "e1981335ce1b0c5adb9511fb716664a9",
+    "url": "/img/search.e1981335.svg"
+  },
+  {
+    "revision": "ff13808150674fce77207e2e17135d3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b77837d187e20c1cc90",
+    "url": "/css/app.b674bbb7.css"
+  }
+];
